9cd4816b2c2eb89a6d515c22a35c3bed0938ee49
Lokeshk <lokesh@gmail.com>
2023-01-08 15:41:40 +0000

    Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ class Hello
 {
 Public static voic main(String args[])
 {
-System.out.println("hell0 from central repositor");
+System.out.println("hell0 from central repositor from child to master");
 }
 }
